e6f9433fc7141e2b2a76761c58cc14a5a87bff23
Ian Bailey <IanKulin@kulin.com.au>
2023-12-31 05:37:50 +0800

    cosmetics

--- src/ExpenseList.tsx
+++ src/ExpenseList.tsx
@@ -2,17 +2,19 @@ import "./ExpenseList.css";
 import { ExpenseProps } from "./types.ts";
 import { useRef, useState } from "react";
 
-
-
 function ExpenseList({ expenses, setExpenses }: ExpenseProps) {
+  if (expenses.length === 0) return null;
+
   const categoryRef = useRef<HTMLSelectElement>(null);
 
   const [selectedCategory, setSelectedCategory] = useState("All");
+
   const handleCategoryChange = (
     event: React.ChangeEvent<HTMLSelectElement>
   ) => {
     setSelectedCategory(event.target.value);
   };
+
   const filteredExpenses = expenses.filter(
     (expense) =>
       selectedCategory === "All" || expense.category === selectedCategory
@@ -32,7 +34,7 @@ function ExpenseList({ expenses, setExpenses }: ExpenseProps) {
           id="category"
           ref={categoryRef}
           name="category"
-          className="form-control"
+          className="form-control drop-down"
           onChange={handleCategoryChange}
         >
           <option value="All">All</option>
@@ -54,10 +56,16 @@ function ExpenseList({ expenses, setExpenses }: ExpenseProps) {
         <tbody>
           {filteredExpenses.map((expense) => (
             <tr key={expense.id}>
-              <td>{expense.description}</td>
-            <td style={{ textAlign: "right" }}>{expense.amount.toFixed(2)}</td>
-              <td>{expense.category}</td>
-              <td>
+              <td className="centred-td">{expense.description}</td>
+              <td style={{ textAlign: "right" }}>
+                $
+                {expense.amount.toLocaleString("en-US", {
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2,
+                })}
+              </td>
+              <td className="centred-td">{expense.category}</td>
+              <td className="centred-td">
                 <button
                   className="btn btn-outline-danger"
                   onClick={() => handleDelete(expense.id)}
@@ -67,16 +75,24 @@ function ExpenseList({ expenses, setExpenses }: ExpenseProps) {
               </td>
             </tr>
           ))}
+        </tbody>
+        <tfoot>
           <tr>
-            <td style={{ fontWeight: "bold" }}>Total</td>
+            <td></td>
             <td style={{ textAlign: "right", fontWeight: "bold" }}>
-                {filteredExpenses.reduce((total, expense) => {
-                    return total + expense.amount;
-                }, 0).toFixed(2)}
+              $
+              {filteredExpenses
+                .reduce((total, expense) => {
+                  return total + expense.amount;
+                }, 0)
+                .toLocaleString("en-US", {
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2,
+                })}
             </td>
             <td colSpan={2}></td>
           </tr>
-        </tbody>
+        </tfoot>
       </table>
     </>
   );
